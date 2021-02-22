604d73714cbe9f5be8a36de77098697ae1c10932
silviadlc <57746795+silviadlc@users.noreply.github.com>
2021-02-22 13:31:41 +0100

    using tailwindCss

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,10 +1,15 @@
-<h3>Crea un nou post</h3>
+@extends('layouts.app')
+@section('content')
+<h3 style="padding: 20px;">Crea un nou post</h3>
+<div style="padding: 20px; width:100%;">
+    <form action="/post" method="post" enctype="multipart/form-data">
+    {{ csrf_field() }}
+        <textarea name="text" rows="10" cols="50">Write something here</textarea>
+        <br><br>
+        <input type="file" name="image">
+        <br><br>
+        <button type="button" class="btn btn-success">Envia</button>
+    </form>
+</div>
+@endsection
 
-<form action="/post" method="post" enctype="multipart/form-data">
-{{ csrf_field() }}
-    <textarea name="text" rows="10" cols="50">Write something here</textarea>
-    <br><br>
-    <input type="file" name="image">
-    <br><br>
-    <input type="submit">
-</form>
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,18 +1,26 @@
-<h1 style="padding: 20;">Hello world!</h1>
-<div style="align-items: center; width:100%; display: grid; grid-template-columns: auto auto auto auto auto;">
-@foreach ($posts as $post)
-    <div class="post" style="text-align: center;">
-        <p style="font-weight: bold; font-size: 20px">{{ $post->text }}</p>
-        <img src="data:image/png;base64,{{$post->image}}" style="max-width: 300px">
-        <div style="">
-            <p>By: {{$post->user->name}}</p>
-            <form action="post/{{$post->id}}" method="post">
-                {{ csrf_field() }}
-                @method('DELETE')
-                <button class="btn btn-danger" type="submit">Elimina</button>
-            </form>
+@extends('layouts.app')
+@section('content')
+
+<h1 class="font-bold m-8 text-3xl text-gray-900 leading-tight">Hello world!</h1>
+<div class="px-20">
+    <div class="flex flex flex-wrap -mx-2">
+    @foreach ($posts as $post)
+        <div class="w-1/5 px-2">
+            <img class="shadow rounded" src="data:image/png;base64,{{$post->image}}" style="max-width: 300px">
+            <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">{{ $post->text }}</div>
+                <div class="flex space-x-32">
+                    <p class="text-gray-700 text-base">By: {{$post->user->name}}</p>
+                    <form action="post/{{$post->id}}" method="post">
+                        {{ csrf_field() }}
+                        @method('DELETE')
+                        <button class="inline-block bg-red-500 hover:bg-white rounded-full px-5 py-1 shadow text-sm font-bold text-white hover:text-red-500 mr-2 mb-2" type="submit">Elimina</button>
+                    </form>
+                </div>
+            </div>
         </div>
+    @endforeach
     </div>
-@endforeach
 </div>
 
+@endsection
