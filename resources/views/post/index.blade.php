ae743bc5a66c81148d1ef66033c91bedd9449a6d
silviadlc <57746795+silviadlc@users.noreply.github.com>
2021-02-22 12:35:15 +0100

    added style in index file

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,15 +1,17 @@
-<h1>Hello world!</h1>
-<div style="width:100%; display: flex">
+<h1 style="padding: 20;">Hello world!</h1>
+<div style="align-items: center; width:100%; display: grid; grid-template-columns: auto auto auto auto auto;">
 @foreach ($posts as $post)
-    <div class="post" style="padding: 20px">
-        <p>{{ $post->text }}</p>
-        <img src="data:image/png;base64,{{$post->image}}" style="max-width:200px">
-        <p>By: {{$post->user->name}}</p>
-        <form action="post/{{$post->id}}" method="post">
-            {{ csrf_field() }}
-            @method('DELETE')
-            <button class="btn btn-danger" type="submit">Elimina</button>
-        </form>
+    <div class="post" style="text-align: center;">
+        <p style="font-weight: bold; font-size: 20px">{{ $post->text }}</p>
+        <img src="data:image/png;base64,{{$post->image}}" style="max-width: 300px">
+        <div style="">
+            <p>By: {{$post->user->name}}</p>
+            <form action="post/{{$post->id}}" method="post">
+                {{ csrf_field() }}
+                @method('DELETE')
+                <button class="btn btn-danger" type="submit">Elimina</button>
+            </form>
+        </div>
     </div>
 @endforeach
 </div>
